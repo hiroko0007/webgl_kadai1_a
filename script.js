b8a8d99c31718fc2644898d161b80c89ab978a13
sudo <104957246+hiroko0007@users.noreply.github.com>
2025-08-10 11:45:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from '/lib/three.module.js';
-import { OrbitControls } from '/lib/OrbitControls.js';
+import * as THREE from './lib/three.module.js';
+import { OrbitControls } from './lib/OrbitControls.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   const wrapper = document.querySelector('#webgl');
@@ -190,3 +190,4 @@ class ThreeApp {
     this.renderer.render(this.scene, this.camera);
   }
 }
+
